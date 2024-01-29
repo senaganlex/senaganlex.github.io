df9ad58b617694ac00ca2c470a041c1620cd3ef6
Xel <45780931+senaganlex@users.noreply.github.com>
2024-01-29 09:08:18 +0800

    Add files via upload

---
+++ AboutUsScript.js
@@ -0,0 +1,4 @@
+function goBackToPortfolio() {
+    // Replace the URL with the correct relative path to your PortfolioActivitySeñagan.html file
+    window.location.href = 'file:///D:/0-VISUAL%20STUDIO%20CODE/Home%20Page/PortfolioActivitySe%C3%B1agan.html';
+}
